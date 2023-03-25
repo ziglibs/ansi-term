c0f55fff039b814fc6b0319b5f3652debdac4e68
Alberto Zanovello <46749229+ZanovelloAlberto@users.noreply.github.com>
2023-03-25 21:32:28 +0000

    added cursor modes (#17)
    * added cursor modes as https://github.com/ziglibs/ansi-term/issues/15
    
    * fmt, and rename the enum

--- src/cursor.zig
+++ src/cursor.zig
@@ -1,7 +1,22 @@
 const std = @import("std");
-
+const testing = std.testing;
+const fixedBufferStream = std.io.fixedBufferStream;
 const esc = "\x1B";
 const csi = esc ++ "[";
+
+pub const CursorMode = enum(u8) {
+    blinking_block = 1,
+    block,
+    blinking_underscore,
+    underscore,
+    blinking_I_beam,
+    I_beam,
+};
+
+pub fn setCursorMode(writer: anytype, mode: CursorMode) !void {
+    const modeNumber = @enumToInt(mode);
+    try writer.print(csi ++ "{d} q", .{modeNumber});
+}
 
 pub fn hideCursor(writer: anytype) !void {
     try writer.writeAll(csi ++ "?25l");
@@ -61,4 +76,28 @@ pub fn scrollUp(writer: anytype, lines: usize) !void {
 
 pub fn scrollDown(writer: anytype, lines: usize) !void {
     try writer.print(csi ++ "{}T", .{lines});
+}
+
+test "test cursor mode BLINKING_UNDERSCORE" {
+    var buf: [1024]u8 = undefined;
+    var fixed_buf_stream = fixedBufferStream(&buf);
+
+    try setCursorMode(fixed_buf_stream.writer(), .blinking_underscore);
+    // the space is needed
+    const expected = csi ++ "3 q";
+    const actual = fixed_buf_stream.getWritten();
+
+    try testing.expectEqualSlices(u8, expected, actual);
+}
+
+test "test cursor mode BLINKING_I_BEAM" {
+    var buf: [1024]u8 = undefined;
+    var fixed_buf_stream = fixedBufferStream(&buf);
+
+    try setCursorMode(fixed_buf_stream.writer(), .blinking_I_beam);
+    // the space is needed
+    const expected = csi ++ "5 q";
+    const actual = fixed_buf_stream.getWritten();
+
+    try testing.expectEqualSlices(u8, expected, actual);
 }
