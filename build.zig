ee1909f4b7529384076661cd9b7c0f7db9fe2cbb
joachimschmidt557 <joachim.schmidt557@outlook.com>
2024-06-03 18:37:23 +0200

    update build.zig to 0.12.0

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     _ = b.addModule("ansi-term", .{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .source_file = .{ .path = "src/main.zig" },
     });
 
     var main_tests = b.addTest(.{
