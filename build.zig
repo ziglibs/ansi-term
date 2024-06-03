9039466b1eeb4944a7a395a716b0ef6c00ddf566
Ved Kothavade <ved@kothavade.com>
2024-06-03 05:16:10 -0400

    b.path

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     _ = b.addModule("ansi-term", .{
-        .source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
     });
 
     var main_tests = b.addTest(.{
