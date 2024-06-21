0bb62115db6749044765fdb37c9791388e7970f2
LordMZTE <lord@mzte.de>
2024-06-21 17:31:51 +0200

    chore: port to Zig 0.13.0

--- build.zig
+++ build.zig
@@ -5,11 +5,11 @@ pub fn build(b: *Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     _ = b.addModule("ansi-term", .{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
     });
 
     var main_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
