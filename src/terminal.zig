5f2ba47fe687c01a5b953cdffa1481e418f0e02f
Danik Vitek <x3665107@gmail.com>
2025-02-28 19:33:39 +0200

    Add terminal state manipulation commands. Remove useless imports

---
+++ src/terminal.zig
@@ -0,0 +1,34 @@
+const esc = "\x1B";
+const csi = esc ++ "[";
+
+pub fn disableLineWrap(writer: anytype) !void {
+    try writer.writeAll(csi ++ "?7l");
+}
+
+pub fn enableLineWrap(writer: anytype) !void {
+    try writer.writeAll(csi ++ "?7h");
+}
+
+pub fn enterAlternateScreen(writer: anytype) !void {
+    try writer.writeAll(csi ++ "?1049h");
+}
+
+pub fn leaveAlternateScreen(writer: anytype) !void {
+    try writer.writeAll(csi ++ "?1049l");
+}
+
+pub fn setSize(writer: anytype, columns: u16, rows: u16) !void {
+    try writer.print(csi ++ "8;{d};{d}t", .{ rows, columns });
+}
+
+pub fn setTitle(writer: anytype, title: []const u8) !void {
+    try writer.print(esc ++ "]0;{s}\x07", .{title.len});
+}
+
+pub fn beginSynchronizedUpdate(writer: anytype) !void {
+    try writer.writeAll(csi ++ "?2026h");
+}
+
+pub fn endSynchronizedUpdate(writer: anytype) !void {
+    try writer.writeAll(csi ++ "?2026l");
+}
